a99a662e61a111c8d026b06d7696741cba1e0b5d
“Yelverarroyave-udea <arroyave.elver@gmail.com>
2024-04-03 16:32:05 -0500

    Ajuste product detail

--- src/app/products/product-detail/product-detail.component.ts
+++ src/app/products/product-detail/product-detail.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
+import {
+  UntypedFormBuilder,
+  UntypedFormGroup,
+  Validators,
+} from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { Product } from 'src/app/model/Product';
 import { AlertService } from 'src/app/services/alert.service';
@@ -11,10 +15,9 @@ import Swal from 'sweetalert2';
 @Component({
   selector: 'app-product-detail',
   templateUrl: './product-detail.component.html',
-  styleUrls: ['./product-detail.component.scss']
+  styleUrls: ['./product-detail.component.scss'],
 })
 export class ProductDetailComponent implements OnInit {
-
   product: Product;
   formProductDetail: UntypedFormGroup;
   inventory: any = {};
@@ -24,67 +27,71 @@ export class ProductDetailComponent implements OnInit {
   categories: Array<any> = [];
 
   constructor(
-    private fb:UntypedFormBuilder,
+    private fb: UntypedFormBuilder,
     private productService: ProductsService,
     private activatedRoute: ActivatedRoute,
     private alert: AlertService,
     private categoryService: CategoryService,
     private inventoryService: InventoryService
-  ) {
-  }
+  ) {}
 
-  ngOnInit(): void{
+  ngOnInit(): void {
     this.loadForm();
     const id: number = parseInt(this.activatedRoute.snapshot.params.id);
-    this.inventoryService.getInventoryById(id).subscribe(data=>{
-      this.inventory = data;
-      this.product = this.inventory.product;
-      this.product.category = this.inventory.product.category?.id;
-      this.priceValue = this.product.price;
-      this.getCategories();
-      this.uploadDates();
-    },err=>{
-      console.log(err)
-    })
+    this.inventoryService.getInventoryById(id).subscribe(
+      (data) => {
+        this.inventory = data;
+        this.product = this.inventory.product;
+        this.product.category = this.inventory.product.category?.id;
+        this.priceValue = this.product.price;
+        this.getCategories();
+        this.uploadDates();
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
   }
 
-  loadForm(){
+  loadForm() {
     //Crear Formulario
     this.formProductDetail = this.fb.group({
-      name:['', Validators.required],
-      code:['', Validators.required],
-      model:[''],
-      brand:['', Validators.required],
-      category:['', Validators.required],
-      price:[100, [Validators.required, Validators.min(100)]],
-      description:[''],
-      amountMountWarranty:[0, Validators.min(0)],
-      weight:[0],
-      height:[0, Validators.min(0)],
-      width:[0, Validators.min(0)],
-      depth:[0, Validators.min(0)],
-      voltage:[0],
-      color:[''],
-      stock:[0],
-      baseStock:[0],
-      createdDate:[''],
-      updatedDate:['']
-    })
-    this.formProductDetail.disable();
+      name: ['', Validators.required],
+      code: ['', Validators.required],
+      model: [''],
+      brand: ['', Validators.required],
+      category: ['', Validators.required],
+      price: [100, [Validators.required, Validators.min(100)]],
+      description: [''],
+      amountMountWarranty: [0, Validators.min(0)],
+      weight: [0],
+      height: [0, Validators.min(0)],
+      width: [0, Validators.min(0)],
+      depth: [0, Validators.min(0)],
+      voltage: [0],
+      color: [''],
+      stock: [0],
+      baseStock: [0],
+      createdDate: [''],
+      updatedDate: [''],
+    });
   }
 
-  getCategories(){
-    this.categoryService.getAllCatetories().subscribe(data=>{
-      this.categories = data;
-    },err=>{
-      console.log(err)
-    })
+  getCategories() {
+    this.categoryService.getAllCatetories().subscribe(
+      (data) => {
+        this.categories = data;
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
   }
 
-  private uploadDates(){
+  private uploadDates() {
     this.formProductDetail.patchValue({
       name: this.product.name,
-      code:this.product.code,
+      code: this.product.code,
       model: this.product.model,
       brand: this.product.brand,
       category: this.product.category,
@@ -100,21 +107,19 @@ export class ProductDetailComponent implements OnInit {
       stock: this.inventory.stock,
       baseStock: this.inventory.baseStock,
       createdDate: this.inventory.createDate,
-      updatedDate: this.inventory.updateDate
+      updatedDate: this.inventory.updateDate,
     });
   }
 
-  isEditable(){
-    this.editable=true;
-    this.formProductDetail.enable();
+  isEditable() {
+    this.editable = true;
   }
 
-  noEditable(){
-    this.editable=false;
-    this.formProductDetail.disable();
+  noEditable() {
+    this.editable = false;
   }
 
-  updateProduct(){
+  updateProduct() {
     const id: number = this.product.id;
     let productToEdit: Product = {
       name: this.formProductDetail.get('name').value,
@@ -123,7 +128,8 @@ export class ProductDetailComponent implements OnInit {
       brand: this.formProductDetail.get('brand').value,
       description: this.formProductDetail.get('description').value,
       category: this.formProductDetail.get('category').value,
-      amountMountWarranty: this.formProductDetail.get('amountMountWarranty').value,
+      amountMountWarranty: this.formProductDetail.get('amountMountWarranty')
+        .value,
       price: this.formProductDetail.get('price').value,
       weight: this.formProductDetail.get('weight').value,
       height: this.formProductDetail.get('height').value,
@@ -133,20 +139,28 @@ export class ProductDetailComponent implements OnInit {
       color: this.formProductDetail.get('color').value,
     };
     console.log('productToEdit-->', productToEdit);
-    this.productService.updateProduct(id, productToEdit).subscribe(data=>{
-      this.alert.successAlet(`¡Producto Editado!`,`${productToEdit.name} editado Correctamente.`)
-      this.noEditable();
-    },err =>{
-      if(err.error.errors != undefined){
-        this.alert.infoAlet(`Error al agregar el producto!`,`${err.error.errors[0].defaultMessage}`)
-      }else(
-        this.alert.infoAlet(`Error al agregar el producto!`,`${err.error}`)
-      )
-    })
+    this.productService.updateProduct(id, productToEdit).subscribe(
+      (data) => {
+        this.alert.successAlet(
+          `¡Producto Editado!`,
+          `${productToEdit.name} editado Correctamente.`
+        );
+        this.noEditable();
+      },
+      (err) => {
+        if (err.error.errors != undefined) {
+          this.alert.infoAlet(
+            `Error al agregar el producto!`,
+            `${err.error.errors[0].defaultMessage}`
+          );
+        } else
+          this.alert.infoAlet(`Error al agregar el producto!`, `${err.error}`);
+      }
+    );
   }
 
-  deleteProduct(){
-    const id: number = this.product.id
+  deleteProduct() {
+    const id: number = this.product.id;
 
     Swal.fire({
       title: `¿Eliminar ${this.product.name}?`,
@@ -154,22 +168,29 @@ export class ProductDetailComponent implements OnInit {
       showCancelButton: true,
       confirmButtonText: `Eliminar`,
       cancelButtonText: 'Cancelar',
-      confirmButtonColor: '#dc3545'
+      confirmButtonColor: '#dc3545',
     }).then((result) => {
       /* Read more about isConfirmed, isDenied below */
       if (result.isConfirmed) {
-        Swal.fire('Eliminado!', '', 'success')
+        Swal.fire('Eliminado!', '', 'success');
 
-        this.productService.deleteProdcut(id).subscribe(data=>{
-          this.alert.infoAlet('¡Producto Eliminar!', `${this.product.name}, eliminado correctamente.`)
-        },err=>{
-          this.alert.errorAlert('Error :/',`Hay un error para eliminar el producto, ${this.product.name}.`)
-        })
-
+        this.productService.deleteProdcut(id).subscribe(
+          (data) => {
+            this.alert.infoAlet(
+              '¡Producto Eliminar!',
+              `${this.product.name}, eliminado correctamente.`
+            );
+          },
+          (err) => {
+            this.alert.errorAlert(
+              'Error :/',
+              `Hay un error para eliminar el producto, ${this.product.name}.`
+            );
+          }
+        );
       } else if (result.isDenied) {
-        Swal.fire('Cambios no guardados', '', 'info')
+        Swal.fire('Cambios no guardados', '', 'info');
       }
-    })
+    });
   }
-
 }
